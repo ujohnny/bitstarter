ecf3ddd7379903bddf48de347e51514336663fd0
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2013-08-06 13:06:49 +0400

    Start with some design & add license

--- web.js
+++ web.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var fs = require('fs');
+
+
+// Configuration
 
 var app = express.createServer(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+// Serve static files like images
+app.use(express.static(__dirname + '/public'));
+
+
+// Routes
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString('utf-8'));
 });
+
+
+// Port
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
