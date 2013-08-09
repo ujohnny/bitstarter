403ad91fcad652c5b236b9b1890e21efaa05e369
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2013-08-09 09:34:52 +0400

    Make the crowfunder design more interesting
    Style index.ejs
    Add FAQ
    Add cool pics
    Tell about the team, add contacts
    Fix navbar
    Show mock concert map

--- web.js
+++ web.js
@@ -7,15 +7,41 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+// Render html pages
+app.set('view options', { layout:false });
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/');
 // Serve static files like images
 app.use(express.static(__dirname + '/public'));
+// Favicon
+app.use(express.favicon(__dirname + '/public/icons/favicon.ico'));
 
 
 // Routes
 
-app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
-  response.send(buffer.toString('utf-8'));
+app.get('/', function(req, res) {
+	res.render('index.ejs');
+});
+
+app.get('/us', function(req, res) {
+	var buffer = fs.readFileSync('us.html');
+	res.send(buffer.toString('utf-8'));
+});
+
+app.get('/demo/1', function(req, res) {
+	res.send("Demo will come soon!".toString('utf-8'));
+});
+
+app.get('/schedule', function(req, res) {
+	res.send("Project schedule will come soon!".toString('utf-8'));
+});
+
+app.get('/blog', function(req, res) {
+	res.send("A cool project blog will come soon!".toString('utf-8'));
+});
+
+app.get('/fame', function(req, res) {
+	res.send("Our future wall of fame".toString('utf-8'));
 });
 
 
